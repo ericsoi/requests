448be5c52c9339e34f0a307f6c61f0a42144f89a
erick.soi <31884872+ericsoi@users.noreply.github.com>
2019-10-08 02:08:46 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,8 +7,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        passwd="jameskinuthia",
-        database="alex"
+        passwd="",
+        database=""
 )
 mycursor = mydb.cursor()
 sql = "insert into data(date, time, imei, companyid, carplate, speed, longitude, longitudedirection, latitude, latitudedirection, powerdisconnection, sygnaldisconnection, Number) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
